c243c63d5561f7eeeb81d156f51036fd39ddfd82
amjad-awad-allah <amjad.awadallah93@gmail.com>
2026-02-23 11:12:22 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+
+public class App{
+    public static void main(String[] args) throws Exception {
+        Liste<String> liste = new Liste<>("KOPF");
+    }
+}
---
+++ src/Element.java
@@ -0,0 +1,25 @@
+public class Element<T> {
+    private T wert;
+    private Element<T> next;
+
+    public Element(T wert, Element<T> next){
+        this.wert = wert;
+        this.next = next;
+    }
+
+    public T getWert() {
+        return wert;
+    }
+
+    public void setWert(T wert) {
+        this.wert = wert;
+    }
+
+    public Element<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Element<T> next) {
+        this.next = next;
+    }
+}
